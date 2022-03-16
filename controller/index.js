11de8bd2df5fa78073dbe1822e4d6bf950759a6b
creeplife <jin.bell@hotmail.com>
2022-03-16 11:12:09 -0300

    16/03

--- controller/index.js
+++ controller/index.js
@@ -2,6 +2,16 @@ const express = require ("express");
 
 const app = express();
 
+app.get ("/", function(req, resp){
+    resp.send("Bem vindos ao meu app");
+})
+app.get("/contato",function(req, resp){
+    resp.send("Página de contato do meu app");
+})
+app.get("/produto",function(req, resp){
+    resp.send("Página de produto do meu app");
+})
+
 //esta sempre dever a ultima linha quando se é usado o express
 app.listen(8081, function(){
     console.log("Servidor funcioando na url http://localhost:8081");
